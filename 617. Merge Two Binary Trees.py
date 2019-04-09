28615cf7093da5ae234dae90e5da99cd04c39025
Annie Shen <annieshenca@gmail.com>
2019-04-08 20:02:42 -0700

    adding LeetCode solutions this time

---
+++ 617. Merge Two Binary Trees.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+        
+        # Store the summed tree back into tree1
+        t1.val += t2.val
+        # Recurse through the left subtrees then right subtrees until all nodes are added up.
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        
+        # Return the summed tree values, in which are all stored in t1.
+        return t1
